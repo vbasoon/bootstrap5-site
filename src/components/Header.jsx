45c11ac058fd99db02201e27f08593b0be0b0770
Басун Володимир <vbasoon@gmail.com>
2020-10-18 11:12:52 +0300

    create dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
    return (
       <div>
          <header id="header" className="fixed-top">
-            <nav className="navbar navbar-expand-lg navbar-dark ">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                <div className="container">
                   <a href="#" className="navbar-brand">BVM Corp</a>
                   <button
@@ -21,13 +21,36 @@ const Header = () => {
                   </button>
                   <div className="collapse navbar-collapse" id="navContent">
                      <ul className="navbar-nav mr-auto mb-3 mb-lg-0">
-                        <li className="nav-item">Item1</li>
+                        <li className="nav-item">
+                           <a href="" className="nav-link">Home</a>
+                        </li>
+                        <li className="nav-item">
+                           <a href="" className="nav-link">Blog</a>
+                        </li>
+                        <li className="nav-item">
+                           <a href="" className="nav-link">About</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                           <a href=""
+                              className="nav-link 
+                           dropdown-toggle"
+                              id="navDrop"
+                              role="button"
+                              data-toggle="dropdown"
+                              aria-expanded="false"
+                           >Services</a>
+                           <ul className="dropdown-menu" aria-labelledby="navDrop">
+                              <li><a href="" className="dropdown-item">Drop1</a></li>
+                              <li><a href="" className="dropdown-item">Drop2</a></li>
+                              <li><a href="" className="dropdown-item">Drop3</a></li>
+                           </ul>
+                        </li>
                      </ul>
                   </div>
                </div>
             </nav>
          </header>
-      </div>
+      </div >
    )
 }
 
